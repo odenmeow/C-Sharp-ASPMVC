d7ffb210251b27fd57aa6071454face0b720d647
Oni <OniLin@universalec.com>
2024-08-01 16:56:09 +0800

    Initial with gitignore to avoid unuse files

---
+++ C-Sharp-ASPMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace C_Sharp_ASPMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ C-Sharp-ASPMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C_Sharp_ASPMVC.MvcApplication" Language="C#" %>
